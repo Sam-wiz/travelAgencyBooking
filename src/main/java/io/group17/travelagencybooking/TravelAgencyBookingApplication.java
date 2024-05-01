dd2d5ed2e2b3181fc25e8515038b767e621ef95e
Sam-wiz <samrudhvvandakudri2005@gmail.com>
2024-05-01 10:57:39 +0530

    Initialised the project

---
+++ src/main/java/io/group17/travelagencybooking/TravelAgencyBookingApplication.java
@@ -0,0 +1,13 @@
+package io.group17.travelagencybooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelAgencyBookingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravelAgencyBookingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/group17/travelagencybooking/TravelAgencyBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package io.group17.travelagencybooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelAgencyBookingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
