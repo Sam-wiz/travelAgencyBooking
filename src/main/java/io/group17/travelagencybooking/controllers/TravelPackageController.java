ad620ff0b1f0640068755bc5ee46a633280a0b8b
ProductServieSST <Pooja.23BCS10151@ms.sst.scaler.com>
2024-05-01 12:52:39 +0530

    Added basic controller package

---
+++ src/main/java/io/group17/travelagencybooking/controllers/TravelPackageController.java
@@ -0,0 +1,11 @@
+package io.group17.travelagencybooking.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/packages")
+public class TravelPackageController {
+
+
+}
