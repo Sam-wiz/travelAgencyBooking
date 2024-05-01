5c780500ca337611c2f0e7c662050123ac984f35
ProductServieSST <Pooja.23BCS10151@ms.sst.scaler.com>
2024-05-01 13:21:27 +0530

    Added Destination Controller

--- src/main/java/io/group17/travelagencybooking/controllers/DestinationController.java
+++ src/main/java/io/group17/travelagencybooking/controllers/DestinationController.java
@@ -1,9 +1,26 @@
 package io.group17.travelagencybooking.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import io.group17.travelagencybooking.models.Destination;
+import io.group17.travelagencybooking.models.TravelPackage;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/destinations")
 public class DestinationController {
+    @GetMapping("/{id}")
+    public Destination getDestinationById(Long id){
+        return null;
+    }
+    @PutMapping("/{id}")
+    public Destination updateDestinationById(@RequestBody Destination destination) {
+        return null;
+    }
+    @PostMapping("")
+    public Destination createDestination(@RequestBody Destination destination){
+        return null;
+    }
+    @DeleteMapping("/{id}")
+    public Destination removeDestinationById(@PathVariable Long id) {
+        return null;
+    }
 }
