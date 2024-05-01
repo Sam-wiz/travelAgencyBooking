88dffc7079e3ea4a01679a7e12b487421182e82c
Sam-wiz <samrudhvvandakudri2005@gmail.com>
2024-05-01 12:58:04 +0530

    Deleted unwanted users

--- src/main/java/io/group17/travelagencybooking/controllers/ProfileController.java
+++
@@ -1,9 +0,0 @@
-package io.group17.travelagencybooking.controllers;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/profiles")
-public class ProfileController {
-}
