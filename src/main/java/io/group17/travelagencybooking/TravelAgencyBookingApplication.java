dc17cb8730fd38ce6459a5ffe4667932901f6e86
Sam-wiz <samrudhvvandakudri2005@gmail.com>
2024-05-01 11:57:59 +0530

    Added user model

--- src/main/java/io/group17/travelagencybooking/TravelAgencyBookingApplication.java
+++ src/main/java/io/group17/travelagencybooking/TravelAgencyBookingApplication.java
@@ -6,7 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TravelAgencyBookingApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         SpringApplication.run(TravelAgencyBookingApplication.class, args);
     }
 
